9aa32cfd193c9710eec92956accc6f6bd66c6747
Patrick Alves <patrickalvesarts@gmail.com>
2025-05-31 00:14:09 -0300

    Funcao conversao

---
+++ scripts.js
@@ -0,0 +1,12 @@
+let form = document.getElementById("converter-form");
+
+
+function convertMoney(){
+
+}
+
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    convertMoney();
+
+})
