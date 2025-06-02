525e0e6597b135283ea605683a6a3f0fa9f9ba54
Patrick Alves <patrickalvesarts@gmail.com>
2025-06-01 22:41:39 -0300

    Api Logica

--- scripts.js
+++ scripts.js
@@ -1,4 +1,15 @@
-let form = document.getElementById("converter-form");
+const form = document.getElementById("converter-form");
+const mount = document.getElementById("amount");
+const fromtCurrency = document.getElementById("fromCurrency");
+const convertedAmount = document.getElementById("convertedAmount");
+const toCurrency = document.getElementById("toCurrency");
+const loading = document.getSelector(".loading");
+const result= document.getSelector(".result");
+const error = document.getSelector(".error");
+
+
+
+// Function to convert money based on user input        
 
 
 function convertMoney(){
